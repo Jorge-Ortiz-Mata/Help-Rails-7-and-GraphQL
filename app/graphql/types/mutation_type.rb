ab2cbe9cb2252202a7bf883893e3f90446d282aa
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2022-10-04 18:41:23 -0500

    One way to create users (mutations) with Ruby on Rails.

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,28 @@
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
+
+    field :create_user, Types::UserType, null: true do
+
+      argument :first_name, String, required: true
+      argument :last_name, String, required: false
+      argument :street, String, required: false
+      argument :number, Int, required: false
+      argument :city, String, required: false
+
+      argument :postcode, Int, required: false
+
+      argument :country, String, required: false
+
     end
+
+    def create_user(first_name:, last_name:, street:, number:, city:, postcode:, country:)
+      User.create(
+                first_name: first_name, last_name: last_name, street: street,
+                number: number, city: city, postcode: postcode, country: country
+      )
+    end
+
+
+
   end
 end

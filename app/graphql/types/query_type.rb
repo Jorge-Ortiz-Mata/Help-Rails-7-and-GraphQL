82512a632252ed8a7f4f83876a9f1c96d1f73467
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2022-10-04 18:06:46 -0500

    Getting all users.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,12 +3,20 @@ module Types
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
+    # --------- Getting Just One User ---------
     field :user, Types::UserType, null: false do
       argument :id, ID, required: true
     end
 
     def user(id:)
       User.find(id)
+    end
+    # -----------------------------------------
+    # --------- Getting all users. -----------
+    field :users, [Types::UserType], null: true
+
+    def users
+      User.all
     end
   end
 end
